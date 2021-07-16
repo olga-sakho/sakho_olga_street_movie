8134f4ad50bc86d925598a19f397ac6e5ec0f529
olga-sakho <sakho.olga93@gmail.com>
2021-07-16 16:17:17 +0300

    change input date and select fields

--- views/script_filter.js
+++ views/script_filter.js
@@ -1,34 +1,29 @@
-var titleSelector = '#title'
-	var titleFilters = document.querySelectorAll(titleSelector)
-		console.log(titleFilters)
-	var timesFilters = document.querySelectorAll('#time')
-		console.log(timesFilters)
-	
-			titleFilters.forEach(function(titleFilter) {
-				console.log(titleFilter)
-				titleFilter.addEventListener('click', function(){
-					console.log('jhnfij')
-					var filterValue = document.querySelectorAll('[data-filter]')
-					console.log(filterValue)
-					for(i=0; i<filterValue.length; i++)
-						var optionValue = filterValue[1].getAttribute('data-filter')
-							console.log('this data filter value', optionValue)
+$(document).ready(function () { 
+ //$('#title').click(function () { 
+  $('#title').on('change', function () { 
+    var selected = $("select option:selected")
+    var titleValue = $(selected).attr('data-filter')
+    var timeObj = $("[data-time]"); 
+     var selectedTime = $('option[data-time="' + titleValue + '"]') 
+      $(timeObj).hide()
+      $(selectedTime).show()
+  });
+  
 
-						timesFilters.forEach(function(timesFilter){
-							var timesValue = document.querySelectorAll('[data-time]')
-								console.log(timesValue)
-							for(i=0; i<timesValue.length; i++) 
-								var timeValue = timesValue[3].getAttribute('data-time')
-									console.log('this data filter value', timeValue)
+  var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0 so need to add 1 to make it 1!
+    var yyyy = today.getFullYear();
+    if(dd<10){
+      dd='0'+dd
+    } 
+    if(mm<10){
+      mm='0'+mm
+    } 
 
-									if(optionValue = timeValue){
-										timesFilter.classList.remove('is-hidden')
-									}
-									else {
-										timesFilter.classList.add('is-hidden')
-									}
-						})	
-						
-					
-		 		});
-			});
+    today = yyyy+'-'+mm+'-'+dd;
+    document.getElementById("date").setAttribute("min", today);
+});
+       
+
+       
